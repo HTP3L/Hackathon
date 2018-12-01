847fde0bc2533298c5cb31b09b7277af5aa5f48d
uselessfulness <mattm@firemail.cc>
2018-12-01 15:13:58 +0000

    Move DB related work while in progress

---
+++ test/Hack.DatabaseTemp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using System.Data;
+using System.Data.SQLite;
+
+namespace Hack.DatabaseTemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection(databaseInfo))
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = query;
+                command.CommandTimeout = 15;
+                command.CommandType = CommandType.Text;
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    reader.Read();
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
