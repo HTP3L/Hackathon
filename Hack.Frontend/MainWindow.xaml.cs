66492f8771d0c2acaabf79f7a55465f20d6d133b
matthew-rimmer <40692294+matthew-rimmer@users.noreply.github.com>
2018-12-02 01:44:27 +0000

    UI Improved
    The UI has be improved to show tabs and to show the map + a datagrid

--- Hack.Frontend/MainWindow.xaml.cs
+++ Hack.Frontend/MainWindow.xaml.cs
@@ -31,6 +31,34 @@ namespace Hack.Frontend
 
             //Add point graphic to graphic overlay
             MapGraphics.Graphics.Add(pointGraphic);
+
+            //Create polygon geometry
+            var polygonPoints = new Esri.ArcGISRuntime.Geometry.PointCollection(SpatialReferences.Wgs84);
+            polygonPoints.Add(new MapPoint(-0.600128, 53.328413));
+            polygonPoints.Add(new MapPoint(-0.549316, 53.342763));
+            polygonPoints.Add(new MapPoint(-0.469666, 53.304621));
+            polygonPoints.Add(new MapPoint(-0.523911, 53.231523));
+            var polygon = new Polygon(polygonPoints);
+
+            System.Drawing.Color blueColorTrans = System.Drawing.Color.FromArgb(128, 0, 0, 255);
+
+
+            //Create symbol for polygon with outline
+            var polygonSymbol = new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, blueColorTrans,
+             new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, redColor, 2));
+
+            //Create polygon graphic with geometry and symbol
+            Graphic polygonGraphic = new Graphic(polygon, polygonSymbol);
+
+            //Add polygon graphic to graphics overlay
+            MapGraphics.Graphics.Add(polygonGraphic);
+
+            MapGraphics.Graphics.Clear();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
