3b4834c5e34bfa30bcdea962736698515008a156
matthew-rimmer <40692294+matthew-rimmer@users.noreply.github.com>
2018-12-02 10:03:06 +0000

    Streamlined UI
    Updated the UI and added draw/clear functions

--- Hack.Frontend/MainWindow.xaml.cs
+++ Hack.Frontend/MainWindow.xaml.cs
@@ -47,63 +47,104 @@ namespace Hack.Frontend
             });
 
 
+            
+
+            
+        }
+
+        private void Clear()
+        {
+            MapGraphics.Graphics.Clear();
+        }
+        private void Draw(bool points, bool area)
+        {
             System.Drawing.Color redColor = System.Drawing.Color.FromName("Red");
             System.Drawing.Color blueColor = System.Drawing.Color.FromName("Blue");
 
-
-            foreach (Incident incident in personOfInterest.Incidents)
+            if (points)
             {
-                //Create a point geometry
-                var point = new MapPoint(incident.XCoord, incident.YCoord, SpatialReferences.Wgs84);
+                foreach (Incident incident in personOfInterest.Incidents)
+                {
+                    //Create a point geometry
+                    var point = new MapPoint(incident.XCoord, incident.YCoord, SpatialReferences.Wgs84);
 
-                
 
-                //Create point symbol with outline
-                var pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, redColor, 10);
-                pointSymbol.Outline = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, blueColor, 2);
 
-                //Create point graphic with geometry & symbol
-                var pointGraphic = new Graphic(point, pointSymbol);
+                    //Create point symbol with outline
+                    var pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, redColor, 10);
+                    pointSymbol.Outline = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, blueColor, 2);
 
-                //Add point graphic to graphic overlay
-                MapGraphics.Graphics.Add(pointGraphic);
-            }
-            
+                    //Create point graphic with geometry & symbol
+                    var pointGraphic = new Graphic(point, pointSymbol);
 
-            
-
-            //Create polygon geometry
-            var polygonPoints = new Esri.ArcGISRuntime.Geometry.PointCollection(SpatialReferences.Wgs84);
-
-            foreach (Incident incident in personOfInterest.Incidents)
-            {
-                polygonPoints.Add(new MapPoint(incident.XCoord, incident.YCoord));
+                    //Add point graphic to graphic overlay
+                    MapGraphics.Graphics.Add(pointGraphic);
+                }
             }
 
-            var polygon = new Polygon(polygonPoints);
-
-            System.Drawing.Color blueColorTrans = System.Drawing.Color.FromArgb(128, 0, 0, 255);
 
 
-            //Create symbol for polygon with outline
-            var polygonSymbol = new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, blueColorTrans,
-             new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, redColor, 2));
 
-            //Create polygon graphic with geometry and symbol
-            Graphic polygonGraphic = new Graphic(polygon, polygonSymbol);
 
-            //Add polygon graphic to graphics overlay
-            MapGraphics.Graphics.Add(polygonGraphic);
+            if (area)
+            {
+                //Create polygon geometry
+                var polygonPoints = new Esri.ArcGISRuntime.Geometry.PointCollection(SpatialReferences.Wgs84);
+
+                foreach (Incident incident in personOfInterest.Incidents)
+                {
+                    polygonPoints.Add(new MapPoint(incident.XCoord, incident.YCoord));
+                }
+
+                var polygon = new Polygon(polygonPoints);
+
+                System.Drawing.Color blueColorTrans = System.Drawing.Color.FromArgb(128, 0, 0, 255);
+
+
+                //Create symbol for polygon with outline
+                var polygonSymbol = new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, blueColorTrans,
+                 new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, redColor, 2));
+
+                //Create polygon graphic with geometry and symbol
+                Graphic polygonGraphic = new Graphic(polygon, polygonSymbol);
+
+                //Add polygon graphic to graphics overlay
+                MapGraphics.Graphics.Add(polygonGraphic);
+            }
+            
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
-
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            Draw((bool)PointBox.IsChecked, (bool)AreaBox.IsChecked);
+        }
 
+        private void PointBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Clear();
+            Draw((bool)PointBox.IsChecked, (bool)AreaBox.IsChecked);
+        }
+
+        private void General_Checked(object sender, RoutedEventArgs e)
+        {
+            Clear();
+            Draw((bool)PointBox.IsChecked, (bool)AreaBox.IsChecked);
+        }
+
+        private void AreaBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Clear();
+            Draw((bool)PointBox.IsChecked, (bool)AreaBox.IsChecked);
+        }
+
+        private void PointBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Clear();
+            Draw((bool)PointBox.IsChecked, (bool)AreaBox.IsChecked);
         }
     }
 }
