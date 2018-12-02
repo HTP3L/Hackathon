c4ea562d500d67b01545c87ef2fddf52a0902695
matthew-rimmer <40692294+matthew-rimmer@users.noreply.github.com>
2018-12-02 02:30:59 +0000

    Configured Datagrid
    Configured the datagrid + created classes for the incidents and persons of interest

---
+++ Hack.Frontend/Incident.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack.Frontend
+{
+    class Incident
+    {
+        public int ID { get; set; }
+        public string TypeOfIncident { get; set; }
+        public double XCoord { get; set; }
+        public double YCoord { get; set; }
+        public string Description { get; set; }
+
+       public Incident()
+        {
+            Random random = new Random();
+            ID = random.Next(0, 9999);
+            TypeOfIncident = "Sighting";
+            Description = "N/A";
+        }
+    }
+}
--- Hack.Frontend/MainWindow.xaml.cs
+++ Hack.Frontend/MainWindow.xaml.cs
@@ -12,32 +12,74 @@ namespace Hack.Frontend
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        PersonOfInterest personOfInterest = new PersonOfInterest();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            //Create a point geometry
-            var point = new MapPoint(-0.497443, 53.256998, SpatialReferences.Wgs84);
+            InterestDatagrid.ItemsSource = personOfInterest.Incidents;
+
+            personOfInterest.Incidents.Add(new Incident
+            {
+                XCoord = -0.600128,
+                YCoord = 53.328413
+            });
+
+
+            personOfInterest.Incidents.Add(new Incident
+            {
+                XCoord = -0.549316,
+                YCoord = 53.342763
+            });
+
+            personOfInterest.Incidents.Add(new Incident
+            {
+                XCoord = -0.469666,
+                YCoord = 53.304621
+            });
+
+            personOfInterest.Incidents.Add(new Incident
+            {
+                XCoord = -0.523911,
+                YCoord = 53.231523
+            });
+
 
             System.Drawing.Color redColor = System.Drawing.Color.FromName("Red");
             System.Drawing.Color blueColor = System.Drawing.Color.FromName("Blue");
 
-            //Create point symbol with outline
-            var pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, redColor, 10);
-            pointSymbol.Outline = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, blueColor, 2);
 
-            //Create point graphic with geometry & symbol
-            var pointGraphic = new Graphic(point, pointSymbol);
+            foreach (Incident incident in personOfInterest.Incidents)
+            {
+                //Create a point geometry
+                var point = new MapPoint(incident.XCoord, incident.YCoord, SpatialReferences.Wgs84);
 
-            //Add point graphic to graphic overlay
-            MapGraphics.Graphics.Add(pointGraphic);
+                
+
+                //Create point symbol with outline
+                var pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, redColor, 10);
+                pointSymbol.Outline = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, blueColor, 2);
+
+                //Create point graphic with geometry & symbol
+                var pointGraphic = new Graphic(point, pointSymbol);
+
+                //Add point graphic to graphic overlay
+                MapGraphics.Graphics.Add(pointGraphic);
+            }
+            
+
+            
 
             //Create polygon geometry
             var polygonPoints = new Esri.ArcGISRuntime.Geometry.PointCollection(SpatialReferences.Wgs84);
-            polygonPoints.Add(new MapPoint(-0.600128, 53.328413));
-            polygonPoints.Add(new MapPoint(-0.549316, 53.342763));
-            polygonPoints.Add(new MapPoint(-0.469666, 53.304621));
-            polygonPoints.Add(new MapPoint(-0.523911, 53.231523));
+
+            foreach (Incident incident in personOfInterest.Incidents)
+            {
+                polygonPoints.Add(new MapPoint(incident.XCoord, incident.YCoord));
+            }
+
             var polygon = new Polygon(polygonPoints);
 
             System.Drawing.Color blueColorTrans = System.Drawing.Color.FromArgb(128, 0, 0, 255);
@@ -52,11 +94,14 @@ namespace Hack.Frontend
 
             //Add polygon graphic to graphics overlay
             MapGraphics.Graphics.Add(polygonGraphic);
-
-            MapGraphics.Graphics.Clear();
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
         }
